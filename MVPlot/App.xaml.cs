c95a4b9a4940e9297ecd881fa8c1229b3a44b01d
AAAA4357 <1113064715@qq.com>
2025-02-08 20:27:11 +0800

    添加安装项目

--- MVPlot/App.xaml.cs
+++ MVPlot/App.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Configuration;
+﻿using HandyControl.Controls;
+using MVPlot.Managers;
+using MVPlot.Utilities;
+using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Windows;
 
 namespace MVPlot
@@ -9,6 +13,15 @@ namespace MVPlot
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if (e.Args.Length == 0) return;
+            string path = e.Args[0];
+            FileInfo info = new(path);
+            PlotProjectUtility.SetPlotProjectFolderPath(info.DirectoryName!);
+            PlotProjectUtility.SetPlotProjectName(info.Name);
+            PlotProjectManager.OpenPlotProject();
+        }
     }
 
 }
